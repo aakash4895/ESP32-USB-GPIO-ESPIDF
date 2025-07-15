009e309918d861b6380483841341437fd6d58f74
Aakash Singh <aakash.singh@forestrock.co.uk>
2025-07-15 10:48:58 +0100

    Initialize project structure with essential configuration files and sample code

---
+++ main/main.c
@@ -0,0 +1,26 @@
+/**
+ * @file main.c
+ * @author Aakash Singh
+ * @brief 
+ * @version 0.1
+ * @date 2025-07-15
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+
+void app_main(void)
+{
+    //Create a new main task
+
+}
